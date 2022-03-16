9a82ddc686b4a1d5cf21a9a34f128b2ab1dc5e69
mehmetsagir <memetsagirint@gmail.com>
2022-03-17 00:23:12 +0300

    add docs website

---
+++ website/theme.config.js
@@ -0,0 +1,18 @@
+export default {
+  github: 'https://github.com/mehmetsagir/step-wizard-react',
+  docsRepositoryBase: 'https://github.com/mehmetsagir/step-wizard-react/',
+  titleSuffix: ' – Step Wizard for React',
+  logo: (
+    <>
+      <span className="mr-2 font-extrabold hidden md:inline">
+        Step Wizard for React
+      </span>
+    </>
+  ),
+  search: false,
+  prevLinks: true,
+  nextLinks: true,
+  footer: true,
+  footerText: 'Step Wizard for React',
+  unstable_faviconGlyph: '👋',
+}
