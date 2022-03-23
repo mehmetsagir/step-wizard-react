22114dde5010b21d20ed242fec286f31a7685ca8
mehmetsagir <memetsagirint@gmail.com>
2022-03-24 01:50:06 +0300

    add chakra-ui

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,5 +1,10 @@
 import 'nextra-theme-docs/style.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
